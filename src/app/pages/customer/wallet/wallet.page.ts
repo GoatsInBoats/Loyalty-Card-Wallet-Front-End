fdf7b85c003c0c4991c788cf6b28e25684adfb7d
AdamPietras <adam.marek.pietras@gmail.com>
2020-07-01 23:59:15 +0200

    created companyservice with method: getCompanies() and userservice with methods: getUserById() and getAllUsers()

--- src/app/pages/customer/wallet/wallet.page.ts
+++ src/app/pages/customer/wallet/wallet.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {Config} from '@ionic/angular';
+import {CompanyService} from '../../../providers/company.service';
+import {UserService} from '../../../providers/user.service';
+import {HttpClient} from '@angular/common/http';
+import {LoginService} from '../../../providers/login.service';
 
 @Component({
   selector: 'app-wallet',
@@ -7,7 +12,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WalletPage implements OnInit {
 
-  constructor() { }
+  segment = 'myCards';
+  groups: any = [];
+  user: any;
+  userId: string;
+  companies: any[];
+
+  constructor(
+      public config: Config,
+      public httpClient: HttpClient,
+      public loginService: LoginService,
+      public companyService: CompanyService,
+      public userService: UserService
+  ) {
+  }
+
+
 
   ngOnInit() {
   }
