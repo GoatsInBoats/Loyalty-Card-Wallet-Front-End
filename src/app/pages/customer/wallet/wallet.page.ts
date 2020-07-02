23347f406d69851e735e4ebfa067cc71d4797949
AdamPietras <adam.marek.pietras@gmail.com>
2020-07-02 23:36:23 +0200

    added wallet.page html and walletpage methods: getCompanies and getItems

--- src/app/pages/customer/wallet/wallet.page.ts
+++ src/app/pages/customer/wallet/wallet.page.ts
@@ -1,35 +1,61 @@
-import { Component, OnInit } from '@angular/core';
-import {Config} from '@ionic/angular';
+/* tslint:disable:no-trailing-whitespace */
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Config, IonList} from '@ionic/angular';
 import {CompanyService} from '../../../providers/company.service';
 import {UserService} from '../../../providers/user.service';
 import {HttpClient} from '@angular/common/http';
 import {LoginService} from '../../../providers/login.service';
+import {any} from 'codelyzer/util/function';
 
 @Component({
-  selector: 'app-wallet',
-  templateUrl: './wallet.page.html',
-  styleUrls: ['./wallet.page.scss'],
+    selector: 'app-wallet',
+    templateUrl: './wallet.page.html',
+    styleUrls: ['./wallet.page.scss'],
 })
 export class WalletPage implements OnInit {
 
-  segment = 'myCards';
-  groups: any = [];
-  user: any;
-  userId: string;
-  companies: any[];
+    @ViewChild('cardsList', {static: true}) cardsList: IonList;
 
-  constructor(
-      public config: Config,
-      public httpClient: HttpClient,
-      public loginService: LoginService,
-      public companyService: CompanyService,
-      public userService: UserService
-  ) {
-  }
+    ios: boolean;
+    segment = 'myCards';
+    groups: any = [];
+    user: any;
+    userId: string;
+    companies: any[];
+    companies1: any;
+
+    constructor(
+        public config: Config,
+        public httpClient: HttpClient,
+        public loginService: LoginService,
+        public companyService: CompanyService,
+        public userService: UserService
+    ) {
+    }
 
 
+    ngOnInit() {
+        this.getCompanies();
+    }
 
-  ngOnInit() {
-  }
+    getCompanies() {
+        this.companyService.getCompanies().subscribe(
+            response => {
+                this.companies1 = response;
+                this.companies = response;
+            }
+        );
+    }
+
+    getItems(ev: any) {
+        this.companies = this.companies1;
+        const val = ev.target.value;
+      // tslint:disable-next-line:triple-equals
+        if (val && val.trim() != '') {
+            this.companies = this.companies.filter((item) => {
+                return (item.companyName.toLowerCase().indexOf(val.toLowerCase()) > -1);
+            });
+        }
+    }
 
 }
