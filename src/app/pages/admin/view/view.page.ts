13d73d5baf21679e7d95f85eedb404d20a645cc5
byrtus <byrtus@wp.pl>
2020-07-15 12:57:10 +0200

    add menu view all users and Companies for admin

---
+++ src/app/pages/admin/view/view.page.ts
@@ -0,0 +1,111 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {Config, IonList} from "@ionic/angular";
+import {CompanyService} from '../../../providers/company.service';
+import {UserService} from "../../../providers/user.service";
+
+@Component({
+  selector: 'app-view',
+  templateUrl: './view.page.html',
+  styleUrls: ['./view.page.scss'],
+})
+export class ViewPage implements OnInit {
+
+  @ViewChild('cardsList', {static: true}) cardList: IonList;
+
+  ios: boolean;
+  segment = 'allUsers';
+  companies: any[];
+  companies1: any[];
+  users: any[];
+  users1: any[];
+  user: any[];
+  search: any = 'admin';
+
+  constructor(
+      public config: Config,
+      public companiesService: CompanyService,
+      public userService: UserService,
+  ) {
+  }
+
+  ionViewDidEnter() {
+    this.ios = this.config.get('mode') === 'ios';
+    this.getCompanies();
+    this.getUsers();
+    this.companies = this.companies1;
+    this.users = this.users1;
+
+  }
+
+  ngOnInit() {
+    this.getCompanies()
+    this.getUsers()
+  }
+
+  getCompanies() {
+    this.companiesService.getCompanies().subscribe(
+        response => {
+          this.companies1 = response;
+          this.companies = response;
+        }
+    );
+  }
+
+  getUsers() {
+    this.userService.getAllUsers().subscribe(
+        response => {
+          this.users1 = response;
+          this.users = response;
+        }
+    );
+  }
+
+
+  getCompaniesItems(ev: any) {
+    this.companies = this.companies1;
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.companies = this.companies.filter((item) => {
+        return (item['companyName'].toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+  }
+
+  getUserItems(ev: any) {
+    this.users = this.users1;
+
+    // set val to the value of the searchbar
+    const val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.users = this.users.filter((item) => {
+        return (item['username'].toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
+
+
+    this.users = this.users.filter((item) => {
+      return (item['roles'].toLowerCase().indexOf(this.search) > -1);
+    });
+  }
+
+  setSearchType(ev: any){
+    this.search = ev.target.value;
+
+    this.getUsersFromType();
+  }
+
+  getUsersFromType() {
+    this.users = this.users1;
+
+    this.users = this.users.filter((item) => {
+      return (item['roles'].toLowerCase().indexOf(this.search) > -1);
+    });
+  }
+
+}
